e9c8567e98f19c9b6bc8be0c6f2d6559431d4781
amit07 <kolpeamit6@gmail.com>
2025-04-04 22:49:19 +0530

    Roman number system

---
+++ roman.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int romanToInt(string s) {
+    unordered_map<char, int> roman = {
+        {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, 
+        {'C', 100}, {'D', 500}, {'M', 1000}
+    };
+
+    int result = 0;
+    int n = s.length();
+
+    for (int i = 0; i < n; i++) {
+        if (i < n - 1 && roman[s[i]] < roman[s[i + 1]]) {
+            result -= roman[s[i]];  
+        } else {
+            result += roman[s[i]]; 
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    
+    cout << romanToInt("III") << endl;      
+    cout << romanToInt("LVIII") << endl;    
+    cout << romanToInt("MCMXCIV") << endl;  
+
+
+    return 0;
+}
